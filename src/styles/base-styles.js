5ac2137068d76b29ffa103d27438b01edd2a6cfc
Mica <mica@digitalyou.es>
2019-06-23 14:27:04 +0200

    Layout the base styles

---
+++ src/styles/base-styles.js
@@ -0,0 +1,47 @@
+
+// DEVICE's SIZES
+const size = {
+  mobile      : '320px',
+  iphone6     : '375px',
+  phablet     : '480px',
+  tablet      : '768px',
+  desktopXs   : '992px',
+  desktopS    : '1024px',
+  desktopM    : '1280px',
+  desktopL    : '1440px',
+  desktopXl   : '1900px',
+  desktopXxl  : '2560px'
+}
+
+export const device = {
+  mobile      : `(min-width: ${size.mobile})`,
+  iphone6     : `(min-width: ${size.iphone6})`,
+  phablet     : `(min-width: ${size.phablet})`,
+  tablet      : `(min-width: ${size.tablet})`,
+  desktopXs   : `(min-width: ${size.desktopXs})`,
+  desktopS    : `(min-width: ${size.desktopS})`,
+  desktopM    : `(min-width: ${size.desktopM})`,
+  desktopL    : `(min-width: ${size.desktopL})`,
+  desktopXl   : `(min-width: ${size.desktopXl})`,
+  desktopXxl  : `(min-width: ${size.desktopXxl})`
+};
+
+export const fontSize = {
+  h1  : '64px',
+  h2  : '48px',
+  h3  : '40px',
+  h4  : '36px',
+  h5  : '32px',
+  h6  : '24px',
+  p   : '16px'
+};
+
+export const lineHeight = {
+  h1  : '48px',
+  h2  : '48px',
+  h3  : '48px',
+  h4  : '48px',
+  h5  : '48px',
+  h6  : '48px',
+  p   : '32px'
+};
---
+++ src/styles/project-custom-styles.js
@@ -0,0 +1,11 @@
+// PROJECT CUSTOM COLORS
+export const colors = {
+  pink      : '#FFA8B0',
+  orange    : '#FFCCAF',
+  lemon     : '#FFF2B2',
+  lime      : '#AEFFAD',
+  lilac     : '#B2C9FF',
+  black     : '#000000',
+  white     : '#FFFFFF',
+  textColor : '#323232'
+};
